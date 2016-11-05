9ad6bb7ac2ebbee043748a0b63bb83d1298703b5
Dhananjay <dj.pandit007@gmail.com>
2016-11-05 12:38:58 -0700

    TV Show Page HTML grabbed

--- nextEpisode.py
+++ nextEpisode.py
@@ -1,2 +1,19 @@
 import urllib2
 from bs4 import BeautifulSoup as bs
+
+def tvShowURL(tvShow):
+    """
+    Takes name of TV show as input and returns a formatted string
+    which can be directly plugged into a URL
+    """
+    tvShow = tvShow.lower()
+    tvShow = tvShow.replace(' ', '-')
+    return tvShow
+
+tvShow = raw_input("Enter TV show name: ")
+tvShow = tvShowURL(tvShow)
+
+url = "http://next-episode.net/" + tvShow
+request = urllib2.Request(url)
+response = urllib2.urlopen(request)
+page = response.read()
