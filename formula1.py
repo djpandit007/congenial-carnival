3d785d3bc03dfc6ce62a61f5f5faed613f323ed0
Dhananjay <dvpandit@asu.edu>
2016-11-07 00:14:25 -0700

    Race, Driver, Constructor URLs grabbed

---
+++ formula1.py
@@ -0,0 +1,21 @@
+import urllib2
+from bs4 import BeautifulSoup as bs
+
+raceURL = "https://www.formula1.com/en/results.html/2016/races.html"
+driversURL = "https://www.formula1.com/en/championship/results/2016-driver-standings.html"
+constructorsURL = "https://www.formula1.com/en/championship/results/2016-constructor-standings.html"
+
+request = urllib2.Request(raceURL)
+response = urllib2.urlopen(request)
+page = response.read()
+raceSoup = bs(page, 'html.parser')
+
+request = urllib2.Request(driversURL)
+response = urllib2.urlopen(request)
+page = response.read()
+driverSoup = bs(page, 'html.parser')
+
+request = urllib2.Request(constructorsURL)
+response = urllib2.urlopen(request)
+page = response.read()
+constructorSoup = bs(page, 'html.parser')
