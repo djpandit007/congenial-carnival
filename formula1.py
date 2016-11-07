a50cd7f59b261824502bb1d270cbe3d8caa20abc
Dhananjay <dvpandit@asu.edu>
2016-11-07 00:17:30 -0700

    Calendar URL added

--- formula1.py
+++ formula1.py
@@ -1,9 +1,15 @@
 import urllib2
 from bs4 import BeautifulSoup as bs
 
+calendarURL = "https://www.formula1.com/en/championship/races/2016.html"
 raceURL = "https://www.formula1.com/en/results.html/2016/races.html"
 driversURL = "https://www.formula1.com/en/championship/results/2016-driver-standings.html"
 constructorsURL = "https://www.formula1.com/en/championship/results/2016-constructor-standings.html"
+
+request = urllib2.Request(calendarURL)
+response = urllib2.urlopen(request)
+page = response.read()
+calendarSoup = bs(page, 'html.parser')
 
 request = urllib2.Request(raceURL)
 response = urllib2.urlopen(request)
