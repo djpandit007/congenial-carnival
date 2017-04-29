ce3018cf72d0efa9cc8c068a88beb920822b9f0f
Dhananjay <dj.pandit007@gmail.com>
2017-04-28 19:51:48 -0700

    Authentication complete

--- TheTvDbEpisode.py
+++ TheTvDbEpisode.py
@@ -1,13 +1,40 @@
-def getAPIKey():
-    # Returns the secret API Key as string
+import requests
+import json
+import sys
+from pprint import pprint
+
+APIURL = "https://api.thetvdb.com"
+
+def getCredentials():
+    # Returns dictionary with credentials
     try:
-        apiFile = open("API Key.txt", "r")
-        apiKey = apiFile.readline()
-        assert apiKey != "", "Invalid API Key"
-        apiFile.close()
+        credentials = open("credentials.txt", "r")
+        apiKey = credentials.readline().strip()
+        assert len(apiKey) == 16, "Invalid API Key"
+        userName = credentials.readline().strip()
+        assert len(userName) == 11, "Invalid username"
+        userKey = credentials.readline().strip()
+        assert len(userKey) == 16, "Invalid user key"
+        credentials.close()
         print "Fetched API Key"
-        return apiKey
+        return {"apikey": apiKey, "username": userName, "userkey": userKey}
     except IOError:
         print "The file with API key not found!"
 
-getAPIKey()
+def authenticate():
+    # Authenticates credentials are returns JWT token
+    data = getCredentials()
+    response = requests.post(APIURL + "/login", json=data)
+    if response.status_code == 401:
+        print "Authentication failure. The program will now quit"
+        sys.exit()
+    elif response.ok and response.status_code == 200:
+        token = json.loads(response.content)["token"]
+        print "JWT Token Generated successfully"
+    else:
+        print "There has been an unexpected error. The program will now quit"
+        sys.exit() 
+    return token
+
+
+authenticate()
