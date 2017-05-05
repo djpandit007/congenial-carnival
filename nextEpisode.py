f1a6c34a669ec00f7788d1be2fe91bc64ea9b147
Dhananjay <dj.pandit007@gmail.com>
2017-05-05 15:30:40 -0700

    Put code in a function

--- nextEpisode.py
+++ nextEpisode.py
@@ -10,29 +10,30 @@ def tvShowURL(tvShow):
     tvShow = tvShow.replace(' ', '-')
     return tvShow
 
-tvShow = raw_input("Enter TV show name: ")
-tvShowFormatted = tvShowURL(tvShow)
+def previousNext(tvShowFormatted):
+    """
+    Takes name of the TV show and returns dictionary about previous and next episodes
+    If no serial is found, raises an exception
+    """
+    url = "http://next-episode.net/" + tvShowFormatted
 
-url = "http://next-episode.net/" + tvShowFormatted
-try:
-    request = Request(url)
-    response = urlopen(request)
-    page = response.read()
-    soup = bs(page, 'html.parser')
+    try:
+        request = Request(url)
+        response = urlopen(request)
+        page = response.read()
+        soup = bs(page, 'html.parser')
 
-    previousEpisode = soup.find('div', {'id': 'previous_episode'}).text
-    previousEpisode = previousEpisode.replace('Summary:Episode Summary', '')\
+        previousEpisode = soup.find('div', {'id': 'previous_episode'}).text
+        previousEpisode = previousEpisode.replace('Summary:Episode Summary', '')\
+                          .replace('\t', '').replace('\n\n', '\n')
+        previousEpisode = previousEpisode.strip()
+
+        nextEpisode = soup.find('div', {'id': 'next_episode'}).text
+        nextEpisode = nextEpisode.replace('Summary:Episode Summary', '')\
                       .replace('\t', '').replace('\n\n', '\n')
-    previousEpisode = previousEpisode.strip()
+        nextEpisode = nextEpisode.strip()
 
-    nextEpisode = soup.find('div', {'id': 'next_episode'}).text
-    nextEpisode = nextEpisode.replace('Summary:Episode Summary', '')\
-                  .replace('\t', '').replace('\n\n', '\n')
-    nextEpisode = nextEpisode.strip()
+        return {"previousEpisode": previousEpisode, "nextEpisode": nextEpisode}
 
-    print previousEpisode
-    print
-    print nextEpisode
-
-except HTTPError:
-    print "Sorry, we could not find the TV series you were looking for! :("
+    except HTTPError:
+        print "Sorry, we could not find the TV series you were looking for! :("
